72b672cc11fd57b142d325c31d9db2ba76401657
Anastasiia Perih <anastasiia.perih@gmail.com>
2022-10-13 13:26:12 +0300

    healthckeck and db/hearbeatCheck

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,28 @@
 const Router = require('koa-router');
+
 const router = new Router();
 
-router.get('/hello', async(ctx) => {
-   ctx.body = 'world!';
-})
+const knex = require('knex');
+const knexfile = require('../../db/knexfile')
+const db = knex(knexfile.development);
+
+router.get('/hello', async (ctx) => {
+  ctx.body = 'world!';
+});
+
+router.get('/healthcheck', async (ctx) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    processtime: process.hrtime(),
+    message: 'OK',
+    timestamp: new Date()
+  };
+
+  try {
+    ctx.body = healthcheck;
+  } catch (error) {
+    healthcheck.message = error;
+    ctx.throw(503);
+  }
+});
 module.exports = router;
