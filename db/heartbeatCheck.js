a74140ea430665a4f6f482891df93db2e433aa7f
Anastasiia Perih <anastasiia.perih@gmail.com>
2022-10-13 13:26:12 +0300

    healthckeck and db/hearbeatCheck

---
+++ db/heartbeatCheck.js
@@ -0,0 +1,11 @@
+const heartbeatCheck = (db) => {
+   db.raw("SELECT 1").then(() => {
+      console.log("PostgreSQL connected");
+    })
+    .catch((e) => {
+      console.log("PostgreSQL not connected");
+      console.error(e);
+    });
+}
+
+module.exports = {heartbeatCheck}
--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
+const knex = require('knex');
+const knexfile = require('../db/knexfile')
+const {Model} = require('objection')
+const {heartbeatCheck} = require('../db/heartbeatCheck')
+
 const Koa = require('koa');
+
 const app = new Koa();
 
 const koaBody = require('koa-body');
-const router = require('./router/router')
+const router = require('./router/router');
 
-app.use(koaBody())
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(koaBody());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const db = knex(knexfile.development);
+Model.knex(db);
+heartbeatCheck(db);
 
 app.listen(3001, () => {
-   console.log('server started')
-})
+  console.log('server started');
+});
